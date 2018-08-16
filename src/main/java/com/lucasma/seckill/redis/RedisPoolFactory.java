5ff95cad922d93a73fd53dbc6d10d942d9132a68
chuckma <robbincen@163.com>
2018-08-16 17:24:31 +0800

    âadd @service

--- src/main/java/com/lucasma/seckill/redis/RedisPoolFactory.java
+++ src/main/java/com/lucasma/seckill/redis/RedisPoolFactory.java
@@ -2,12 +2,14 @@ package com.lucasma.seckill.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author Administrator
  */
+@Service
 public class RedisPoolFactory {
 
     @Autowired
